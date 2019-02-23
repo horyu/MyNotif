381f84856969b9b2d5cc1b905ad5567ccdabe4f8
horyu <28817604+horyu@users.noreply.github.com>
2019-02-23 11:18:57 +0900

    トーストのエラーを回避：文字列の受信に長さ制限を追加

--- MyNotif/NotifySever.cs
+++ MyNotif/NotifySever.cs
@@ -44,19 +44,27 @@ namespace MyNotif
             {
                 stream.ReadTimeout = 5000;
 
-                byte[] resBytes = new byte[1024];
+                const int maxLen = 512 * 4; // 適当：通知幅的に表示できる文字数 * 1文字当たりのバイト数
+                byte[] resBytes = new byte[maxLen];
                 int resSize = 0;
                 using (MemoryStream ms = new MemoryStream())
                 {
                     do
                     {
-                        //一部を受信 → msに蓄積
+                        // 一部を受信 → msに蓄積
                         resSize = await stream.ReadAsync(resBytes, 0, resBytes.Length).ConfigureAwait(false);
                         ms.Write(resBytes, 0, resSize);
-                        //ストリーム死or受信長0で終わり
+                        // maxLenバイト以上受信 で読み込み終わり
+                        if (ms.Length >= maxLen)
+                        {
+                            break;
+                        }
+                        // ストリーム死 or 受信長0（ストリームの末尾） で読み込み終わり
                     } while (stream.DataAvailable || (resSize >= 1));
 
-                    string resMsg = Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                    int readLength = System.Math.Min((int)ms.Length, maxLen);
+                    // バッファーの末尾がマルチバイト文字の途中の場合、切り捨て
+                    string resMsg = Encoding.UTF8.GetString(ms.GetBuffer(), 0, readLength);
                     toaster.Fire(resMsg);
                 }
             }
